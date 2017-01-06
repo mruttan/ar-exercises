196c1601455270b6178d97b02a2e60d4376b775b
Matt Ruttan <matthew.ruttan5@gmail.com>
2017-01-06 23:17:22 +0000

    Exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,20 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+class Store < ActiveRecord::Base
+  validates_length_of :name, minimum: 3
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+end
+
+class Employees < ActiveRecord::Base
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_inclusion_of :hourly_rate, in: 10..99999
+  validates_presence_of :store_id
+end
+
+puts "Input a Store Name!"
+@user_input = gets.chomp
+
+
+Store.create(name: @user_input)
