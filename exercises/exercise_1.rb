890aae453ebfc177abbc130ef98d40ba13a9f0b9
Matt Ruttan <matthew.ruttan5@gmail.com>
2017-01-06 23:03:30 +0000

    Exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,8 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+Store.create(name: 'Sears', annual_revenue: 1000000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Zellers', annual_revenue: 0, mens_apparel: true, womens_apparel: true)
+Store.create(name: 'Metro', annual_revenue: 500000, mens_apparel: false, womens_apparel: false)
+
+puts Store.count
